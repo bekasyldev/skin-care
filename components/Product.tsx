00074127e015be1d4ae2faf9785280c07446e4c1
Bekasyl <devbekasyl@gmail.com>
2023-11-20 19:59:35 +0600

    database migration

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import React from "react";
-import { productData } from "@/constants/data";
 import { Button } from "./ui/button";
+import { db } from "@/lib/db";
 
-const Product = () => {
+export default async function Product() {
+  const products = await db.product.findMany({});
   return (
     <div className="grid grid-cols-4 gap-y-16 gap-x-4">
-      {productData.map((product, index) => (
-        <div key={index} className="flex flex-row items-center">
+      {products.map((product) => (
+        <div key={product.id} className="flex flex-row items-center">
           <div className="flex flex-col space-y-2">
             <div className="bg-white border w-[300px] h-[350px] flex items-center justify-center">
               Image
             </div>
             <div className="space-y-2">
               <div className="flex justify-between font-medium">
-                <span>{product.name}</span>
+                <span>{product.title}</span>
                 <span>${product.price}</span>
               </div>
               <p className="text-xs">{product.variant}</p>
@@ -27,6 +28,4 @@ const Product = () => {
       ))}
     </div>
   );
-};
-
-export default Product;
+}
